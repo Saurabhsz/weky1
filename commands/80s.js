91a125d1a854348b7abf08e99d2348eb5bf911b8
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-20 17:10:15 +0200

    Update 80s.js

--- commands/80s.js
+++ commands/80s.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
 const member = message.mentions.members.first() || message.member;
 const avatar = member.user.displayAvatarURL({ format: 'jpg' });
-const att = `https://api.monkedev.com/canvas/80s?imgUrl${avatar}=&key=scNyfoysHunZd79reAL5VEsQV.jpg`
+const att =     `https://api.monkedev.com/fun/8ball?key=scNyfoysHunZd79reAL5VEsQV`
     const attachment = new Discord.MessageAttachment(att , `80s.jpg`);
     message.channel.send(attachment);
 }
