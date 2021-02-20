d6ca5e41a2244e08064ccf7522bcaa66f45d8ba0
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-20 18:45:18 +0200

    Update sepia.js

--- commands/sepia.js
+++ commands/sepia.js
@@ -3,8 +3,8 @@ const Canvas = require('canvas');
 module.exports.run = async (bot, message, args) => {
 
 const member = message.mentions.members.first() || message.member;
-const avatar = member.user.displayAvatarURL();
-const att = `https://api.monkedev.com/canvas/sepia?imgUrl=${avatar}=scNyfoysHunZd79reAL5VEsQV`
+const avatar = member.user.displayAvatarURL({ format: 'jpg' });
+const att = `https://api.monkedev.com/canvas/sepia?key=scNyfoysHunZd79reAL5VEsQV&imgUrl=${avatar}`
     const attachment = new Discord.MessageAttachment(att , `sepia.jpg`);
     message.channel.send(attachment);
 }
