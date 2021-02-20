be4c4b4dbe6bff93951ad8956c5855d44d7dc887
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-20 14:41:05 +0200

    e

--- commands/80s.js
+++ commands/80s.js
@@ -4,15 +4,15 @@ module.exports.run = async (bot, message, args) => {
 
 const member = message.mentions.members.first() || message.member;
 const avatar = member.user.displayAvatarURL({ format: 'jpg' });
-const att = `https://api.monkedev.com/other/gav?key=scNyfoysHunZd79reAL5VEsQV&imgUrl=${avatar}`
-    const attachment = new Discord.MessageAttachment(att , `bird.jpg`);
+const att = `https://api.monkedev.com/canvas/80s?imgUrl${avatar}=&key=scNyfoysHunZd79reAL5VEsQV`
+    const attachment = new Discord.MessageAttachment(att , `80s.jpg`);
     message.channel.send(attachment);
 }
 
 module.exports.config = {
-    name: "bird",
-    description: "your's or the pinged user's pfp in a bird buddy",
-    usage: "/bird @user",
+    name: "80s",
+    description: "80s vibe go brrrrrr",
+    usage: "/80s @user",
     accessableby: "Members",
-    aliases: ["gav"]
+    aliases: ["80seconds"]
 }
