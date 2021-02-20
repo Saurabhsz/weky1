8b33e1aa5e7a8dc496d7e69fecd8b262c37ea4f3
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-20 12:20:51 +0200

    e

--- commands/poll.js
+++ commands/poll.js
@@ -1,11 +1,14 @@
 const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
+        if (!member.hasPermission('MANAGE_MESSAGES')) {
+                message.reply(`Get manage messages permission and then come back lmao.`)
+        }
 let pollChannel = message.mentions.channels.first();
         let pollDescription = args.slice(2).join(' ');
 if(!pollChannel) return message.reply(`Please mention the channel where the poll you want to be`)
 if(!pollDescription) return message.reply(`What you want to put in this poll :D?`)
         let embedPoll = new Discord.MessageEmbed()
-        .setTitle('😲 New Poll! 😲')
+        .setTitle(`😲 ${message.author.username} Made a new POLL`)
         .setDescription(pollDescription)
         .setColor('RANDOM')
         let msgEmbed = await pollChannel.send(embedPoll);
