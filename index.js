347e0144c6a7cc41d8005c430ece0ed739038861
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-21 13:34:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ fs.readdir("./commands/", (err, files) => {
 });
 bot.on("message", async message => {
     if(message.content === '/lb'){
-    const leaderboardData = require('../schemas/Money')
+    const leaderboardData = require('./schemas/Money')
 async function getLeaderobard() {
     var replyString = "";
     await leaderboardData.findOne({
