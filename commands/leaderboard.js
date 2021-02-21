228c5b4931a08b6eada92ef1b2b29be5a4d6d0b3
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-21 10:47:54 +0200

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,15 +1,17 @@
 module.exports.run = async (bot, message, args) => {
-MongoClient.connect("mongodb+srv://eusuntgabi:eusuntgabi@cluster0.0bpkf.mongodb.net/Data", function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("Data");
-    var sortSelection = { Wallet: 1 };
-  
-    dbo.collection("moneydatas").find().sort(sortSelection).toArray(function(err, result) {
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb+srv://eusuntgabi:eusuntgabi@cluster0.0bpkf.mongodb.net/Data";
+    
+    MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      console.log(result);
-      db.close();
+      var dbo = db.db("Data");
+      var mysort = { Wallet: 1 };
+      dbo.collection("moneydatas").find().sort(mysort).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+      });
     });
-  }); 
 }
   module.exports.config = {
     name: "lb",
