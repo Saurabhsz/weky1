2cd3e6a0baebc915157a8aa8cebe74f8ae4088ec
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-21 11:09:08 +0200

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,13 +1,11 @@
 module.exports.run = async (bot, message, args) => {
     const mongo = require("mongoose");
     
-    mongo.setURL("mongodb+srv://eusuntgabi:eusuntgabi@cluster0.0bpkf.mongodb.net/Data"); // You only need to do this ONCE per process.
-
-    const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10); // We grab top 10 users with most xp in the current server.
+    const rawLeaderboard = await mongo.fetchLeaderboard(message.guild.id, 10); // We grab top 10 users with most xp in the current server.
      
     if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
      
-    const leaderboard = await Levels.computeLeaderboard(bot, rawLeaderboard, true); // We process the leaderboard.
+    const leaderboard = await mongo.computeLeaderboard(bot, rawLeaderboard, true); // We process the leaderboard.
      
     const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}${e.Wallet.toLocaleString()}`);
      
