c83aeaf6f83444d67b2c981858eb106bb87aa2e7
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-21 11:06:51 +0200

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,17 +1,17 @@
 module.exports.run = async (bot, message, args) => {
-    var MongoClient = require('mongoose').MongoClient;
-    var url = "mongodb+srv://eusuntgabi:eusuntgabi@cluster0.0bpkf.mongodb.net/Data";
+    const mongo = require("mongoose");
     
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("Data");
-      var mysort = { Wallet: 0 };
-      dbo.collection("moneydatas").find().sort(mysort).toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        db.close();
-      });
-    });
+    mongo.setURL("mongodb+srv://eusuntgabi:eusuntgabi@cluster0.0bpkf.mongodb.net/Data"); // You only need to do this ONCE per process.
+
+    const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10); // We grab top 10 users with most xp in the current server.
+     
+    if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
+     
+    const leaderboard = await Levels.computeLeaderboard(bot, rawLeaderboard, true); // We process the leaderboard.
+     
+    const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}${e.Wallet.toLocaleString()}`);
+     
+    message.channel.send(`**Leaderboard**:\n\n${lb.join("\n\n")}`);
 }
   module.exports.config = {
     name: "lb",
