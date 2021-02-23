75127e481a8def22035d4ea770da5430f6b2da76
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-23 14:44:44 +0200

    Update dep.js

--- commands/dep.js
+++ commands/dep.js
@@ -25,7 +25,7 @@ Money.findOne({
   } else if(num > data.Wallet) {
     return message.channel.send("You dont have that much money why you bad at me bro :/");
   }
-  var thesame = data.space===data.space
+  var thesame = data.space==data.space
   if(thesame){
     return message.channel.send(`Looks like your bank is full :) go get some scripts`)
 
