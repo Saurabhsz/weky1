def878ff0ff9854e9ac553fa9fca338c040aa390
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-25 21:19:29 +0200

    e

--- commands/eval.js
+++ commands/eval.js
@@ -11,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
 
         try {
 
-            const toEval = args.join(" ");
+            const toEval = args.slice(1).join(" ");
             const evaluated = eval(toEval);
 
             const embed = new MessageEmbed()
