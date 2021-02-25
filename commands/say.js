cc8a25d33b4c66857e2a5e4ed822eaf6e05272bd
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-25 21:14:45 +0200

    e

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require("discord.js")
 module.exports.run = async (client, message, args) => {
-    if (!args[0]){
+    if (!args[1]){
         return message.channel.send('What should i say?')
     }
     message.channel.send(args.slice(0).join(" ") + `\n_ _ _ _ _ _ - ${message.author.username}#${message.author.discriminator}`)
