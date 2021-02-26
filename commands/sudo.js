3831275360113a6e0d8420d3f930dd02293c8dce
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-26 17:10:22 +0200

    e

---
+++ commands/sudo.js
@@ -0,0 +1,25 @@
+const {
+    Client,
+    Message,
+    MessageEmbed
+} = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+        if (!args[1]) return message.reply('Mention someone you doofus!')
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        if (!member) return message.reply(`Couldn't find this user!`)
+
+        message.channel.createWebhook(member.user.username, {
+            avatar: member.user.displayAvatarURL({ dynamic: true })
+        }).then(webhook => {
+            webhook.send(args.slice(1).join(" "))
+            setTimeout(() => {
+                webhook.delete()
+            }, 3000)
+        })
+}
+module.exports.config = {
+    name: "sudo",
+    usage: "/sudo @mention <text>",
+    accessableby: "Members",
+    aliases: []
+}
