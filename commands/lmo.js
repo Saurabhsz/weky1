b820e3ef53a36d5e61a7f62239e749fe5343111a
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-27 08:21:23 +0200

    Update lmo.js

--- commands/lmo.js
+++ commands/lmo.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js")
 module.exports.run = async (client, message, args) => {
     const embed = new Discord.MessageEmbed()
-    .setTitle(`<:prayge:812975011107307521> — Chat Healer`)
-    .setDescription(`React to this message if you want to get notified when the chat of this server is dead`)
+    .setTitle(`<:Angery:806544377987465216> — Giveaway Ping`)
+    .setDescription(`React to this message if you want to get notified when there will be a giveaway`)
     message.channel.send(embed)
 }
 module.exports.config = {
