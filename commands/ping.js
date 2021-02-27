5075c7c779d08133bd4ca1383f9e89e7aa9df2ea
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-27 08:27:45 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,14 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args, client) => {
-      message.channel.send(`Pong! \`${bot.ws.ping}\``)
+    const msg = await message.channel.send("Pinging...");
+
+    const apiLatency = Math.round(client.ws.ping);
+    const botLatency = msg.createdTimestamp - message.createdTimestamp;
+    
+    // Delete the 'Pinging...' message
+
+    // Send 'Pong!' with the api and the bot latency
+    msg.edit(`Pong! API latency is **${apiLatency}ms**, bot latency is **${botLatency}ms**`);
 }
 
 module.exports.config = {
