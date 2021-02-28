bcf52f5d7fe0f470a29bd0100b8f861691ff14e6
ImFace <78975742+ImFace@users.noreply.github.com>
2021-02-28 08:23:17 +0200

    e

---
+++ commands/warnings.js
@@ -0,0 +1,36 @@
+const warns = require("../schemas/wanings");
+const { MessageEmbed } = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+  let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]);
+  if (!user) return message.channel.send(`No user specified!`);
+    warns.find(
+      { GuildID: message.guild.id, UserID: user.id },
+      async (err, data) => {
+        if (err) console.log(err);
+        if (!data.length)
+          return message.channel.send(
+            `${user.user.tag} doen\` have any warnings in this server.`
+          );
+        let Embed = new MessageEmbed()
+          .setTitle(`${user.user.tag}'s warns in ${message.guild.name}.. `)
+          .setDescription(
+            data.map((d) => {
+              return d.Punishments.map(
+                (w, i) =>
+                  `${i} - \`Moderator:\` ${
+                    message.guild.members.cache.get(w.Moderator).user.tag
+                  } \`Reason:\` ${w.Reason}`
+              ).join("\n");
+            })
+          );
+        message.channel.send(Embed);
+      }
+    );
+  }
+  module.exports.config = {
+    name: "warnings",
+    description: "shows someone's warnings",
+    usage: "/warnings @user",
+    accessableby: "Members",
+    aliases: ["warns"]
+}
---
+++ schemas/wanings.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+let ModSchema = new mongoose.Schema({
+    GuildID: String,
+    UserID: String,
+    Punishments: Array
+})
+const MessageModel = module.exports = mongoose.model('Warnings', ModSchema)
