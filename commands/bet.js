f47d633ba95ee8831fcc8bd4cd13a72cf837fda1
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-01 12:37:43 +0200

    Update bet.js

--- commands/bet.js
+++ commands/bet.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
     const Money = require('../schemas/Money')
     const rndom = Math.floor(Math.random() * 100) + 1; // get a random num between 1 and 100
       const hance = rndom > 0 && rndom < 70; // the conidtion which is the chance
-      if(message.content.includes('.') || message.content.includes(',')) return message.channel.send(`In your message CAN'T be . and ,`)
+      if(message.content.includes(',')) return message.channel.send(`In your message CAN'T be . and ,`)
 
     var number = parseFloat(args[1])
     if(!number) return message.channel.send(`Please specify how much you want to bet!`)
