2971e9599db088a8153ac3e46dd4ea64e4ca3191
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-01 07:35:15 +0200

    e

---
+++ commands/doggo.js
@@ -0,0 +1,20 @@
+
+
+const Discord = require('discord.js');
+const Canvas = require('canvas');
+module.exports.run = async (bot, message, args) => {
+const member = message.mentions.members.first() || message.member;
+    const canvas = Canvas.createCanvas(500, 670);
+    const ctx = canvas.getContext('2d');
+    const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/779450313723084870/815636172579405844/image0.jpg');
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({format: 'jpg' }));
+    ctx.drawImage(avatar, 250, 50, 205, 205);
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), `doggo_${member.user.username}.jpg`);
+    message.channel.send(attachment);
+}
+
+module.exports.config = {
+    name: "doggo",
+    aliases: []
+}
