bc804446c28056415105fb402459eca2c3035c6c
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-02 09:09:29 +0200

    E

--- commands/help.js
+++ commands/help.js
@@ -3,13 +3,47 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let helpArray = message.content.split(" ");
     let helpArgs = helpArray.slice(1);
-
+    if(helpArgs[0] === 'games') {
+        let em = new Discord.MessageEmbed()
+        .addField('🎮 Games', '`trivia`, `fight`, `fasttype`')
+        return message.channel.send(em)
+    }
+    if(helpArgs[0] === 'fun') {
+        let em = new Discord.MessageEmbed()
+        .addField('😂 Fun', '\`corona`, `rc`, `meme`, `simprate`, `ship`, `noobrate`, `fart`, `cringerate`, `8ball`, `avatar`, `google`, `emoji`, `pp`, `trivia`, `ascii`, `reverse`, `say`, `sudo`, `emojify\`\n')
+        return message.channel.send(em)
+    }
+    if(helpArgs[0] === 'currency') {
+        let em = new Discord.MessageEmbed()
+        .addField('💰 Currency', '`beg`, `buy`, `shop`, `dep`, `with`, `daily`, `bet`, `bal`, `use`, `share`, `inventory`, `pog`')
+        return message.channel.send(em)
+    } 
+    if(helpArgs[0] === 'currency') {
+        let em = new Discord.MessageEmbed()
+        .addField('⚒️ Moderation', '`purge`, `lockdown`, `endlockdown`, `warn`, `warnings`, `ban`')
+        return message.channel.send(em)
+    } 
+    if(helpArgs[0] === 'utility') {
+        let em = new Discord.MessageEmbed()
+        .addField('🔩 Utility', '`ping`, `invite`, `uptime`, `math`, `poll`, `report`, `serveremoji`, `addemoji`')
+        return message.channel.send(em)
+    } 
+    if(helpArgs[0] === 'image') {
+        let em = new Discord.MessageEmbed()
+        .addField('📷 Image', '`drip`, `gray`, `blur`, `facepalm`, `hitler`, `invert`, `gay`, `trigger`, `rip`, `shit`, `trash`, `bird`, `sepia`, `dither`, `80s`, `pet`, `alone`, `muffin`, `toilet`, `turtle` `sock`, `moment`, `doggo`, `awesome`')
+        return message.channel.send(em)
+    } 
     //Normal usage of (prefix)help without any args. (Shows all of the commands and you should set the commands yourself)
     if(!helpArgs[0]) {
         var embed = new Discord.MessageEmbed()
         .setTitle('Weky Bot Help')
         .setURL('https://www.youtube.com/channel/UCZ_cuJGBis0vi6U3bWmvDIg')
-        .setDescription('😂 Fun:\n `corona`, `rc`, `meme`, `simprate`, `ship`, `noobrate`, `fart`, `cringerate`, `snipe`, `8ball`, `avatar`, `google`, `emoji`, `pp`, `trivia`, `ascii`, `reverse`, `say`, `fasttype`, `sudo`, `emojify`\n💰 Currency:\n `beg`, `buy`, `shop`, `dep`, `with`, `daily`, `bet`, `bal`, `use`, `share`, `inventory`, `pog`\n⚒️ Moderation:\n `purge`, `lockdown`, `endlockdown`, `warn`, `warnings`, `ban`\n🔩 Utility:\n `ping`, `invite`, `uptime`, `math`, `poll`, `report`, `serveremoji`, `addemoji`\n📷 Image:\n`drip`, `gray`, `blur`, `facepalm`, `hitler`, `invert`, `gay`, `trigger`, `rip`, `shit`, `trash`, `bird`, `sepia`, `dither`, `80s`, `pet`, `alone`, `muffin`, `toilet`, `turtle` `sock`, `moment`, `doggo`, `awesome`')
+        .addField("😂 Fun", "`..help fun`\n")
+        .addField("💰 Currency", "`..help currency`\n")
+        .addField("⚒️ Moderation", "`..help moderation`\n")
+        .addField("🔩 Utility", "`..help utility`\n")
+        .addField("🎮 Games", "`..help games`\n")
+        .addField("📷 Image", "`..help image`\n")
         .setColor('#FF2D00')
         .setThumbnail('https://cdn.discordapp.com/emojis/798365870148681729.png?v=1')
         .setTimestamp()
