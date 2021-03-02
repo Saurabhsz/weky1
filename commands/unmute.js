2bb2819417065fcd8a4fdbb9567e4abf8147f778
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-02 11:01:24 +0200

    24-24

---
+++ commands/unmute.js
@@ -0,0 +1,22 @@
+var ms = require('ms')
+module.exports.run = async (bot, message, args) => {
+
+    let Discord = require('discord.js')
+//checking if the member has the mute perms
+if (!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("You can not use this command");
+//checking if the bot has the mute perms
+if (!message.guild.me.hasPermission("MUTE_MEMBERS")) return message.channel.send("I do not have \`MUTE_MEMBERS\` permission.");
+if (message.guild.id !== '795393018764591134') return message.reply('This command can only be used in another server.');
+const mentionedMember = message.mentions.members.first();
+
+const muteRole = message.guild.roles.cache.find(role => role.id === '814569175284318270')
+if (!mentionedMember) return message.channel.send("Please state a member that is in the server to mute");
+
+    await mentionedMember.roles.remove(muteRole).then(message.channel.send(`<@${mentionedMember.id}> has been unmuted`));
+    await mentionedMember.send(`You have been unmuted`).catch(err => console.log(err));
+
+    }
+        module.exports.config = {
+            name: "unmute",
+            aliases: []
+        }
