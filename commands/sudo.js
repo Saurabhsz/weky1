a69f984d689414639228c05290db8bb56e1b767b
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-03 15:18:38 +0200

    Update sudo.js

--- commands/sudo.js
+++ commands/sudo.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
         if (!member) return message.reply(`Couldn't find this user!`)
 
-        message.channel.createWebhook(member.user.username, {
+        message.channel.createWebhook("FAKE | "+member.user.username, {
             avatar: member.user.displayAvatarURL({ dynamic: true })
         }).then(webhook => {
             webhook.send(args.slice(2).join(" "))
