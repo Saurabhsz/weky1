17b166f4bf91a04c9a8389e4406a338d8184f36b
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-03 10:17:08 +0200

    Update rul.js

--- commands/rul.js
+++ commands/rul.js
@@ -6,17 +6,17 @@ module.exports.run = async (bot, message, args) => {
             const Embed = new MessageEmbed()
             .setAuthor(bot.user.displayAvatarURL())
             .setDescription(`By using Weky bot, you agree of following all these rules. Breaking one will result the access from any command.`)
-            .addField(`[RULE 1](https://www.youtube.com/)`,
-                       `\`Alts Bank\`\nDon't use any alts (other accounts) to deposit the coins or items and keep them safe.`)
-            .addField(`[RULE 2](https://www.youtube.com/)`,
-                       `\`Raiding\`\nDon't use the bot for a raid of commands or for a spam with the echo commands.`)
-            .addField(`[RULE 3](https://www.youtube.com/)`,
-                       `\`Scripts, Auto Grind\`\nSelf bots, scripts
+            .addField(`<:empty:816584899502538755>`,
+                       `[RULE 1](https://www.youtube.com/)\n\`Alts Bank\`\nDon't use any alts (other accounts) to deposit the coins or items and keep them safe.`)
+            .addField(`<:empty:816584899502538755>`,
+                       `[RULE 2](https://www.youtube.com/)\n\`Raiding\`\nDon't use the bot for a raid of commands or for a spam with the echo commands.`)
+            .addField(`<:empty:816584899502538755>`,
+                       `[RULE 3](https://www.youtube.com/)\n\`Scripts, Auto Grind\`\nSelf bots, scripts
                        Usage of user-bots, macros, scripts, auto-typers or anything else enabling auto grinding are causing a lot of spam and they are not allowed.`)
-            .addField(`[RULE 4](https://www.youtube.com/)`,
-                       `\`Real money trading\`\nNo items or anything from our currency selling for real things or money.`)
-            .addField(`[RULE 5](https://www.youtube.com/)`,
-                       `\`Sharing bugs\`\nSharing any bug about this bot with the other users. Please keep the bugs only for you and use the ..report command for us to see it.`)
+            .addField(`<:empty:816584899502538755>`,
+                       `[RULE 4](https://www.youtube.com/)\n\`Real money trading\`\nNo items or anything from our currency selling for real things or money.`)
+            .addField(`<:empty:816584899502538755>`,
+                       `[RULE 5](https://www.youtube.com/)\n\`Sharing bugs\`\nSharing any bug about this bot with the other users. Please keep the bugs only for you and use the ..report command for us to see it.`)
         message.channel.send(Embed);
 
 }
