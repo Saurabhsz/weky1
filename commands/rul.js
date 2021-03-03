bd54c40bfbe61a83bf99c8c105449640b42c2c25
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-03 10:08:54 +0200

    Update rul.js

--- commands/rul.js
+++ commands/rul.js
@@ -6,16 +6,16 @@ module.exports.run = async (bot, message, args) => {
             const Embed = new MessageEmbed()
             .setAuthor(bot.user.displayAvatarURL())
             .setDescription(`By using Weky bot, you agree of following all these rules. Breaking one will result the access from any command.`)
-            .addField(`[RULE 1](https://discord.com/oauth2/authorize?client_id=809496186905165834&scope=bot&permissions=2083912822)
+            .addField(`[RULE 1](https://www.youtube.com/)
                        \n\`Alts Bank\`\nDon't use any alts (other accounts) to deposit the coins or items and keep them safe.`)
-            .addField(`[RULE 2](https://discord.com/oauth2/authorize?client_id=809496186905165834&scope=bot&permissions=2083912822)
+            .addField(`[RULE 2](https://www.youtube.com/)
                        \n\`Raiding\`\nDon't use the bot for a raid of commands or for a spam with the echo commands.`)
-             .addField(`[RULE 3](https://discord.com/oauth2/authorize?client_id=809496186905165834&scope=bot&permissions=2083912822)
+            .addField(`[RULE 3](https://www.youtube.com/)
                        \n\`Scripts, Auto Grind\`\nSelf bots, scripts
                        Usage of user-bots, macros, scripts, auto-typers or anything else enabling auto grinding are causing a lot of spam and they are not allowed.`)
-             .addField(`[RULE 4](https://discord.com/oauth2/authorize?client_id=809496186905165834&scope=bot&permissions=2083912822)
+            .addField(`[RULE 4](https://www.youtube.com/)
                        \n\`Real money trading\`\nNo items or anything from our currency selling for real things or money.`)
-             .addField(`[RULE 5](https://discord.com/oauth2/authorize?client_id=809496186905165834&scope=bot&permissions=2083912822)
+            .addField(`[RULE 5](https://www.youtube.com/)
                        \n\`Sharing bugs\`\nSharing any bug about this bot with the other users. Please keep the bugs only for you and use the ..report command for us to see it.`)
         message.channel.send(Embed);
 
