ffee7e625fd935314a95dae8a2bbcfda704c6891
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-03 10:21:57 +0200

    Update rul.js

--- commands/rul.js
+++ commands/rul.js
@@ -7,16 +7,16 @@ module.exports.run = async (bot, message, args) => {
             .setAuthor(bot.user.displayAvatarURL())
             .setDescription(`By using Weky bot, you agree of following all these rules. Breaking one will result the access from any command.`)
             .addField(`<:empty:816585596788539412>`,
-                       `[RULE 1](https://www.youtube.com/)\n\`Alts Bank\`\nDon't use any alts (other accounts) to deposit the coins or items and keep them safe.`)
+                       `[𝗥𝗨𝗟𝗘 𝟭](https://www.youtube.com/)\n\`Alts Bank\`\nDon't use any alts (other accounts) to deposit the coins or items and keep them safe.`)
             .addField(`<:empty:816585596788539412>`,
-                       `[RULE 2](https://www.youtube.com/)\n\`Raiding\`\nDon't use the bot for a raid of commands or for a spam with the echo commands.`)
+                       `[𝗥𝗨𝗟𝗘 𝟮](https://www.youtube.com/)\n\`Raiding\`\nDon't use the bot for a raid of commands or for a spam with the echo commands.`)
             .addField(`<:empty:816585596788539412>`,
-                       `[RULE 3](https://www.youtube.com/)\n\`Scripts, Auto Grind\`\nSelf bots, scripts
+                       `[𝗥𝗨𝗟𝗘 𝟯](https://www.youtube.com/)\n\`Scripts, Auto Grind\`\nSelf bots, scripts
                        Usage of user-bots, macros, scripts, auto-typers or anything else enabling auto grinding are causing a lot of spam and they are not allowed.`)
             .addField(`<:empty:816585596788539412>`,
-                       `[RULE 4](https://www.youtube.com/)\n\`Real money trading\`\nNo items or anything from our currency selling for real things or money.`)
+                       `[𝗥𝗨𝗟𝗘 𝟰](https://www.youtube.com/)\n\`Real money trading\`\nNo items or anything from our currency selling for real things or money.`)
             .addField(`<:empty:816585596788539412>`,
-                       `[RULE 5](https://www.youtube.com/)\n\`Sharing bugs\`\nSharing any bug about this bot with the other users. Please keep the bugs only for you and use the ..report command for us to see it.`)
+                       `[𝗥𝗨𝗟𝗘 𝟱](https://www.youtube.com/)\n\`Sharing bugs\`\nSharing any bug about this bot with the other users. Please keep the bugs only for you and use the ..report command for us to see it.`)
         message.channel.send(Embed);
 
 }
