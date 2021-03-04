17af34448e3574d62475259516dd31407dc70bc4
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-04 15:20:07 +0200

    Update invetory.js

--- commands/invetory.js
+++ commands/invetory.js
@@ -22,7 +22,7 @@ if(data.fishing != 0) resp += `**<:plastichand:816373822784667719> Plastic Hand 
 if(data.fish != 0) resp += `**<:fish:816367967733547038> Fish - ** ${data.fish}\n`
 if(data.fishrare != 0) resp += `**<:rare_fish:816368038110035999> Rare Fish - ** ${data.fishrare}\n`
 if(data.silvermoon != 0) resp += `**<:silver_moon:816983800260067338> Silver Moon - ** ${data.silvermoon}\n`
-if(data.Lootbox != 0) resp += `**<:gotcha_box:816990104803475457> Silver Moon - ** ${data.Lootbox}\n`
+if(data.Lootbox != 0) resp += `**<:gotcha_box:816990104803475457> Gotcha Lootbox - ** ${data.Lootbox}\n`
 const { MessageEmbed } = require("discord.js");
         const Embed = new MessageEmbed()
         .setTitle(`${message.author.username}\' Inventory`)
