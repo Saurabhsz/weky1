7bc256d50d2998bd78d48af43c22c4ad9cdc2a87
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-04 08:48:09 +0200

    Update fish.js

--- commands/fish.js
+++ commands/fish.js
@@ -22,55 +22,52 @@ module.exports.run = async (bot, message, args) => {
             if(0 >= data.fishing) {
         return message.channel.send(`U dont have a Plastic Hand what a poor`);
         } else {
-    let random = Math.floor(Math.random() * 3) +1
-                const repp = [
-                    `You got <:fish:816367967733547038> ${random} fishes by fishing with your Plastic Hand, very cool.`,
-                    `You got <:fish:816367967733547038> ${random} fishes by fishing with your Plastic Hand`,
-                    `You got <:fish:816367967733547038> ${random} fishes by fishing with your Plastic Hand, noice`,
-                    `You got <:fish:816367967733547038> ${random} fishes by fishing with your Plastic Hand, for literally nothing`,
-                    `You got <:fish:816367967733547038> ${random} fishes, but your <:plastichand:816373822784667719> Plastic Hand broke :/`,
-                    `You died because a fish ate your eye, and lost all, idk bro you seems sus but atleast you still have the <:plastichand:816373822784667719> Plastic Hand`,
-                    `You got <:fish:816367967733547038> ${random} fishes after nothing`,
-                    `You didnt caught anything, noob Wekier`,
-                    `You didnt caught anything, noob`,
-                    `You didnt caught anything, noob`,
-                    `You didnt caught anything, noob`,
-                    `You didnt caught anything, noob`,
-                    `You didnt caught anything, noob`,
-                    `A big gg dude, you got <:rare_fish:816368038110035999> 1 Rare Fish`
-               ]
-               var xxxx = Math.floor(Math.random() * repp.length);
-               const answerspos = repp[xxxx]
-               if(xxxx == 0 && xxxx == 1 && xxxx == 2 && xxxx == 3 && xxxx == 6){ 
-                data.fish += random
-                data.save();
-            }
-               if(xxxx == 4){
-               data.fish += random
-               data.fishing -= 1
-               data.save();
-            }
-               if(xxxx == 5){
-                data.Wallet -= data.Wallet
-                data.save();
-                }
-               if(xxxx == 7 && xxxx == 8 && xxxx == 9 && xxxx == 10 && xxxx == 11 && xxxx == 12){ 
-                   return
-                }
-                if(xxxx == 13) { data.fishrare += 1 
-                data.save()
-            }
+            let random = Math.floor(Math.random() * 3)+1
+            const repp = [
+                `You got <:fish:816367967733547038> ${random} fishes by fishing with your Plastic Hand, very cool.`,
+                `You got <:fish:816367967733547038> ${random} fishes by fishing with your Plastic Hand`,
+                `You got <:fish:816367967733547038> ${random} fishes by fishing with your Plastic Hand, noice`,
+                `You got <:fish:816367967733547038> ${random} fishes by fishing with your Plastic Hand, for literally nothing`,
+                `You got <:fish:816367967733547038> ${random} fishes, but your <:plastichand:816373822784667719> Plastic Hand broke :/`,
+                `You died because a fish ate your eye, and lost all, idk bro you seems sus but atleast you still have the <:plastichand:816373822784667719> Plastic Hand`,
+                `You got <:fish:816367967733547038> ${random} fishes after nothing`,
+                `You didnt caught anything, noob Wekier`,
+                `You didnt caught anything, noob`,
+                `You didnt caught anything, noob`,
+                `You didnt caught anything, noob`,
+                `You didnt caught anything, noob`,
+                `You didnt caught anything, noob`,
+                `A big gg dude, you got <:rare_fish:816368038110035999> 1 Rare Fish`
+    
+            ]
+           var xxxx = Math.floor(Math.random() * repp.length);
+           const answerspos = repp[xxxx]
+           if(xxxx == 0){ data.fish += random;}
+           if(xxxx == 1){ data.fish += random;}
+           if(xxxx == 2){ data.fish += random;}
+           if(xxxx == 3){ data.fish += random;}
+           if(xxxx == 4){ data.fish += random; data.fishing -= 1}
+           if(xxxx == 5){ data.Wallet -= data.Wallet;}
+           if(xxxx == 6){ data.fish += random}
+           if(xxxx == 7){ return}
+           if(xxxx == 8){ return}
+           if(xxxx == 9){ return}
+           if(xxxx == 10){ return}
+           if(xxxx == 11){ return}
+           if(xxxx == 12){ return}
+           if(xxxx == 13){ data.fishrare += 1}
+
                message.reply(`${answerspos}`)
                data.save();
             }
            
         }
     })
-        usedCommand.add(message.author.id);
-        setTimeout(() => {
-            usedCommand.delete(message.author.id);
-        }, 45000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
-    }
+    talkedRecently.add(message.author.id);
+    setTimeout(() => {
+    talkedRecently.delete(message.author.id);
+    }, 45000);
+}
 }
 
 module.exports.config = {
