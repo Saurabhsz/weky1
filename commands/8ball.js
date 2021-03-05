f9e2a1330ee5977d5d4688521667505f3012ab2b
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-05 18:50:57 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const Canvas = require('canvas');
 module.exports.run = async (bot, message, args) => {
-    if(args[1]) return message.reply(`Please put a valid question <:ah:811587999380799498>.`)
+    if(!args[1]) return message.reply(`Please put a valid question <:ah:811587999380799498>.`)
     const fetch = require('node-fetch')
     const res = await (await (fetch(`https://api.monkedev.com/fun/8ball?key=scNyfoysHunZd79reAL5VEsQV`))).json();
     const b = new Discord.MessageEmbed()
