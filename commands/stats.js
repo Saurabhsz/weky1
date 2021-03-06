9d47cc78e62aa7bcd9dc16e3ec44c26672443370
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-06 12:16:31 +0200

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     const msg = await message.reply(`Loading...`).then(m  => m.delete(), 1000)
 let embed = new Discord.MessageEmbed()
 .setAuthor(`Weky bot stats`)
-.addField(`__Cache__\n`,`**Users**: ${bot.users.cache.size}\n**Guilds**: ${bot.guilds.cache.size} \n\n`, true)
+.addField(`__Cache__\n`,`**Guilds**: ${bot.guilds.cache.size} \n\n`, true)
 .addField(`__Uptime__\n`,`**Bot**: ${bot.uptime}\n**Process**: ${prettyMs(process.uptime() * 1000)}`, true)
 .addField(`__Movement__\n`,`**API Latency**: ${Math.round(bot.ws.ping)}\n**Bot Latency**: ${msg.createdTimestamp - message.createdTimestamp}ms`, true)
 message.reply(embed)
