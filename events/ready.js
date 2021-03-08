b003b942d1f07a9bdd19863f8e7501956e694916
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-08 16:17:12 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,12 @@
-const Discord = require("discord.js")
-
-module.exports = bot => { 
-    console.log(`${bot.user.username} is online`)
-    bot.user.setActivity(`Playing in ${bot.guilds.size}`, {type: "PLAYING"});
+//ready event (ready.js)
+module.exports = {
+    name: 'ready',
+    once: true,
+    execute(client, commandFiles, folder) {
+        commandFiles.forEach(cmd => {
+            console.log('> ' + `${folder} | ` + `${cmd}`)
+        })
+        console.log(`----------------------------------------\n¡${client.user.tag} has been successfully connected!`)
+        bot.user.setActivity(`Playing in ${bot.guilds.size}`, {type: "PLAYING"});
+    }
 }
