b29cc9b838e679946d3f44c559a38b3a634015e5
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-10 09:34:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,71 +43,71 @@ for (const file of eventFiles) {
     	}
     }
 } 
-
-//Event - message
-bot.on("message", async message => {
+if(message.author.bot || message.channel.type === "dm") return;
     const black = require('./schemas/Ban')
     black.findOne({
       id: message.author.id
     }, (err,data) => {
       if(err) console.log(err);
       if(!data){
-    if(message.author.bot || !message.content.startsWith(prefix)) return;
-    const args = message.content.slice(prefix.length).split(/ +/g);
-    if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
-    const commandName = args.shift().toLowerCase();
+        if(message.author.bot || !message.content.startsWith(prefix)) return;
+        const args = message.content.slice(prefix.length).split(/ +/g);
+        if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
+        const commandName = args.shift().toLowerCase();
+    
+        const cmd = bot.commands.get(commandName)
+            //+ aliases: [""],
+            || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    
+            try{
+    if(!cmd) return;
+                //+ cooldown 1, //seconds(s)
+                if (!cooldowns.has(cmd.name)) {
+                    cooldowns.set(cmd.name, new Discord.Collection());
+                }
+                
+                const now = Date.now();
+                const timestamps = cooldowns.get(cmd.name);
+                const cooldownAmount = (cmd.cooldown || 3) * 1000;
+                
+                if (timestamps.has(message.author.id)) {
+                    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+                
+                    if (now < expirationTime) {
+                        const timeLeft = (expirationTime - now) / 1000;
+                        const embed = new Discord.MessageEmbed()
+                        .setAuthor(`Wait god damn`)
+                        .setDescription(`😡 This command have a cooldown, not like your life, wait \`${timeLeft.toFixed(1)}s\``)
+                        .setFooter(`This timer dont update in real time`)
+                        message.channel.send(embed);
+                    } 
+                } else {timestamps.set(message.author.id, now);
+                  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+      
+              //+ args: true/false,
+                       
+                      //+ guildOnly: true/false,
+                      if (cmd.guildOnly && message.channel.type === 'dm') {
+                          return message.reply('I can\'t execute that command inside DMs!');
+                      }
+      
+                      //+ dmOnly: true/false,
+                      if (cmd.dmOnly && message.channel.type === 'text') {
+                          return message.reply('I can\'t execute that command inside the server!');
+                      }
+      
+              cmd.execute(bot, message, args);}
+                
+        }catch(err){
+            message.reply("`\❌ ERROR`\n ```css\n" +  err + "\n```")
+        }
 
-    const cmd = bot.commands.get(commandName)
-        //+ aliases: [""],
-        || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+      } else {
+return;
+      }
+    });
+//Event - message
 
-        try{
-if(!cmd) return;
-            //+ cooldown 1, //seconds(s)
-            if (!cooldowns.has(cmd.name)) {
-                cooldowns.set(cmd.name, new Discord.Collection());
-            }
-            
-            const now = Date.now();
-            const timestamps = cooldowns.get(cmd.name);
-            const cooldownAmount = (cmd.cooldown || 3) * 1000;
-            
-            if (timestamps.has(message.author.id)) {
-                const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-            
-                if (now < expirationTime) {
-                    const timeLeft = (expirationTime - now) / 1000;
-                    const embed = new Discord.MessageEmbed()
-                    .setAuthor(`Wait god damn`)
-                    .setDescription(`😡 This command have a cooldown, not like your life, wait \`${timeLeft.toFixed(1)}s\``)
-                    .setFooter(`This timer dont update in real time`)
-                    message.channel.send(embed);
-                } 
-            } else {timestamps.set(message.author.id, now);
-              setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-  
-          //+ args: true/false,
-                   
-                  //+ guildOnly: true/false,
-                  if (cmd.guildOnly && message.channel.type === 'dm') {
-                      return message.reply('I can\'t execute that command inside DMs!');
-                  }
-  
-                  //+ dmOnly: true/false,
-                  if (cmd.dmOnly && message.channel.type === 'text') {
-                      return message.reply('I can\'t execute that command inside the server!');
-                  }
-  
-          cmd.execute(bot, message, args);}
-            
-    }catch(err){
-        message.reply(`there was an error in the console.`);
-        console.log(err);
-    }
-}else{
-    return;
-}
-})
 
 
 
