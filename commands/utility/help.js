66eb070c1e86917a3e49c894b464a62fb39c3731
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-11 16:51:37 +0200

    Update help.js

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -82,8 +82,8 @@ module.exports = {
             Command name: **${command.name || "No name"}**\n
             Command usage: **${command.usage || "No Usage"}**\n
             Command aliases: ${command.aliases}\n
-            Command cooldown: **${command.cooldown || "0"}**\n
-            Command premium cooldown: **${command.cooldowny || "0"}**\n
+            Command cooldown: **${command.cooldown || "0"}s**\n
+            Command premium cooldown: **${command.cooldowny || "0"}s**\n
             Command permissions: **${command.permissions || "none"}**
             `)
             .setColor('RANDOM')
