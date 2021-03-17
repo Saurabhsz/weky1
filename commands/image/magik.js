c5eb984b615ee460163c21f74e41b7a7db3efd59
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-17 12:40:21 +0200

    ok

--- commands/image/magik.js
+++ commands/image/magik.js
@@ -10,12 +10,19 @@ module.exports = {
     guarded: true, //or false
     permissions: ["NONE"],
     async execute(bot, message, args) {
-        const Discord = require('discord.js');
-        const Idiot = require("idiotic-api");
-        bot.API = new Idiot.Client(process.env.token);
-        if(!args[0]) return message.channel.send(`Well you cant get the achievement for nothing`)
-        await message.channel.send(new Discord.MessageAttachment(
-            await bot.API.achievement(message.author.displayAvatarURL({ format: "png", size: 32 }), args.slice(0).join(" ")),
-            "achievement.png"));
+        const { createCanvas, loadImage, registerFont } = require('canvas');
+const path = require('path');
+const { shortenText } = require('../../util/Canvas');
+registerFont(path.join(__dirname, '..', '..', 'assets', 'fonts', 'Minecraftia.ttf'), { family: 'Minecraftia' });
+		const base = await loadImage(path.join(__dirname, '..', '..', 'assets', 'images', 'achievement.png'));
+		const canvas = createCanvas(base.width, base.height);
+		const ctx = canvas.getContext('2d');
+		ctx.drawImage(base, 0, 0);
+		ctx.font = '17px Minecraftia';
+		ctx.fillStyle = '#ffff00';
+		ctx.fillText('Achievement Get!', 60, 40);
+		ctx.fillStyle = '#ffffff';
+		ctx.fillText(shortenText(ctx, text, 230), 60, 60);
+		return msg.say({ files: [{ attachment: canvas.toBuffer(), name: 'achievement.png' }] });
 }
 }
