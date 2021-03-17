78876a368f2409f0491a3c491aa8f8038a39ceaf
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-17 12:16:46 +0200

    achievement

--- commands/image/magik.js
+++ commands/image/magik.js
@@ -3,63 +3,21 @@
 const Discord = require('discord.js');
 const Canvas = require('canvas');
 module.exports = {
-    name: "magik",
+    name: "achievement",
     aliases: [],
     dmOnly: false, //or false
     guildOnly: true, //or false
-    usage: '..magik',
+    usage: '..achievement',
     cooldown: 4, //seconds(s)
     cooldowny: 0,
     guarded: true, //or false
     permissions: ["NONE"],
     async execute(bot, message, args) {
-        const fetch = require('node-fetch')
-        if (message.mentions.users.size) {
-            let member = message.mentions.users.first()
-            if (member) {
-              let URL = (member.avatarURL({ format: 'png', dynamic: false, size: 1024 }))
-              let POWER = args.slice(0).join(" ")
-      if(!POWER){
-          return message.channel.send(`Please specify the intensity of the image magik effect(1-10)`)
-      }
-              fetch(`https://nekobot.xyz/api/imagegen?type=magik&image=${URL}&intensity=${POWER}&raw=1`)
-                .then(res => res.json())
-                .then(data => {
-                    let embed = new Discord.MessageEmbed()
-                    .setColor("RANDOM")
-                    .setImage(data.message)
-                    .setTitle(`Click here for link`)
-                    .setURL(data.message)
-                    .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL())
-                    .setTimestamp()
-                  message.channel.send(embed)
-      
-                })
-      
-            } else {
-              message.channel.send("Sorry none found with that name")
-            }
-      
-      
-          } else {
-            let imageURL = (message.author.avatarURL({ format: 'png', dynamic: false, size: 1024 }))
-            let poweruser = args.slice(0).join(" ")
-            if(!poweruser){
-                return message.channel.send(`Please specify the intensity of the image magik effect(1-10)`)
-            }
-            fetch(`https://nekobot.xyz/api/imagegen?type=magik&image=${imageURL}&intensity=${poweruser}&raw=1`)
-              .then(res => res.json())
-              .then(data => {
-                let embed = new Discord.MessageEmbed()
-                  .setColor("RANDOM")
-                  .setImage(data.message)
-                  .setTitle(`Click here for link`)
-                  .setURL(data.message)
-                  .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL())
-                  .setTimestamp()
-                message.channel.send(embed)
-      
-              })
-          }
+        const Idiot = require("idiotic-api");
+        bot.API = new Idiot.Client(process.env.token);
+        if(!args[0]) return message.channel.send(`Well you cant get the achievement for nothing`)
+        await message.channel.send(new MessageAttachment(
+            await client.API.achievement(message.author.displayAvatarURL({ format: "png", size: 32 }), args.slice(0).join(" ")),
+            "achievement.png"));
 }
 }
