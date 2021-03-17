81f489742f99bd700b711799d011e150db906e3b
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-17 12:00:38 +0200

    Update magik.js

--- commands/image/magik.js
+++ commands/image/magik.js
@@ -16,8 +16,12 @@ module.exports = {
         const fetch = require('node-fetch')
         const e = Math.floor(Math.random() * 11) +1
         const member = message.mentions.members.first() || message.member;
-const avatar = member.user.displayAvatarURL({ format: 'jpg' });
-const att = await fetch(encodeURI(`https://nekobot.xyz/api/imagegen?type=magik&image=${avatar}&intensity=${e}&raw=1`));
+        const avatar = member.avatarURL({
+            format: 'png',
+            dynamic: false,
+            size: 1024
+          })
+          const att = await fetch(encodeURI(`https://nekobot.xyz/api/imagegen?type=magik&image=${avatar}&intensity=${e}&raw=1`));
 const attachment = new Discord.MessageAttachment(att , `magik.jpg`);
     message.channel.send(attachment);
 }
