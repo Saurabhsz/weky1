0ae8b9b5915659cb9d0b04311555e2cac03953ff
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-23 15:59:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/dblwebhook', wh.middleware(), (req, res) => {
   console.log(req.vote.user) // => 321714991050784770
 })
 
-app.listen(80)
+app.listen(3000)
 const Topgg = require("@top-gg/sdk")
 const api = new Topgg.Api('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgwOTQ5NjE4NjkwNTE2NTgzNCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjE2Mzk2MzU4fQ.nvMPl_16_Lo9jiwKLfohjVNPOJGNL3rLAav_hwaYtb0')
 setInterval(() => {
