cd7b63342b547c1a6d3dee6e0512baa6cc46d2e6
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-23 11:45:58 +0200

    Create troll.js

---
+++ commands/utility/troll.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "tr",
+    aliases: [],
+    dmOnly: false, //or false
+    guildOnly: true, //or false
+    usage: '..tr',
+    cooldown: 4, //seconds(s)
+    cooldowny: 0,
+    guarded: true, //or false
+    permissions: ["NONE"],
+    async execute(bot, message, args) {
+        message.delete()
+const Discord = require("discord.js")
+ var embed = new Discord.MessageEmbed()
+.setTitle("Support Weky Message")
+.addField("Support Me!", "click [here](https://pnrtscr.com/ck08e6)" )
+.setColor("303030")
+message.send(embed).then(mesasge.channel.send(args[0]))
+    }}
