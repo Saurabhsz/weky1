9ed3fc7702e14460c305e45190f07a2249007087
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-26 10:31:21 +0200

    Update add.js

--- commands/currency/add.js
+++ commands/currency/add.js
@@ -9,12 +9,15 @@ module.exports = {
   guarded: true, //or false
   permissions: ["NONE"],
   async execute(bot, message, args) {
-    console.log(message.author.username + ` used this command`)
-
+    if(message.author.id !== "778518819055861761" || message.author.id !== "700988024770789376" || message.author.id !== "619498919763640330") return;
       const member = message.mentions.members.first()
       if(!member) return message.channel.send(`No user specified`)
-      const number = parseInt(args[1])
+      const number = parseInt(args[0])
         if(!number) return message.reply(`No amount`)
+        bot.channels,cache.get("824272046174765096").send(new Discord.MessageEmbed()
+        .setAuthor(message.author.username+`#`+message.author.discriminator, message.member.user.displayAvatarURL())
+        .setDescription(`Added to ${member.id} some coins`)
+        .setFooter(Number))
         message.reply(`Added to ${member.username} ${number} coins :)`)
         bot.add(member.id, number)
   }}
