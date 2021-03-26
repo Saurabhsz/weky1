53e53ecaacad00bb1ba04c841dff929cca9d032b
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-26 10:59:16 +0200

    bfuh

--- commands/currency/add.js
+++ commands/currency/add.js
@@ -12,7 +12,7 @@ module.exports = {
     if(message.author.id === "778518819055861761" || message.author.id === "700988024770789376" || message.author.id === "619498919763640330"){
       const member = message.mentions.members.first()
       if(!member) return message.channel.send(`No user specified`)
-      const number = parseInt(args[0])
+      const number = Math.round(parseInt(args[0]))
         if(!number) return message.reply(`No amount`)
         bot.channels.cache.get("824272046174765096").send(new Discord.MessageEmbed()
         .setAuthor(message.author.username+`#`+message.author.discriminator, message.member.user.displayAvatarURL())
