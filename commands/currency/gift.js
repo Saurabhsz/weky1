1e04f536451ea343fe81cf5e68e8245f3db83dd8
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-30 20:23:57 +0300

    Update gift.js

--- commands/currency/gift.js
+++ commands/currency/gift.js
@@ -23,7 +23,9 @@ module.exports = {
 if(!validItem) return message.channel.send(`This item is not a real item :rolling_eyes:`)
 const itemIcon = items.find((val) => val.aliases.includes(itemToBuy)).emoji
 const validName = items.find((val) => val.aliases.includes(itemToBuy)).realItem
-if(bot.item(message.author.id, validName) < num) {return message.reply(`You dont have ${num} ${realItem} bruh`)}else{
+const dat = require('../../schemas/inventory')
+const data = dat.findOne({ User: message.author.id })
+if(data[validName] < num) {return message.reply(`You dont have ${num} ${realItem} bruh`)}else{
 bot.addItem(member.id, validName, num)
 bot.removeItem(message.author.id, validName, num)
 message.reply(message.author.username + ` gave ` + member.user.tag + ` ` + itemIcon + ` ` + num + ` ` + validName)
