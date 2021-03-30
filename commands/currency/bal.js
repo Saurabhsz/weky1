5de845ecd3e7bc13422560b09795e904b306811b
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-30 14:29:31 +0300

    Update bal.js

--- commands/currency/bal.js
+++ commands/currency/bal.js
@@ -12,7 +12,7 @@ module.exports = {
     const target = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0])
 if(!target){
     let embed = new Discord.MessageEmbed()
-    .addField(message.author.user.tag + '\'s balance\n', "**Wallet**: " + await bot.bal(target.id) + "\n**Bank**: " + await bot.Bank(target.id) + "/" + await bot.space(target.id))
+    .addField(message.author.username + '\'s balance\n', "**Wallet**: " + await bot.bal(target.id) + "\n**Bank**: " + await bot.Bank(target.id) + "/" + await bot.space(target.id))
     .setColor("RANDOM")
     .setTimestamp();
     message.channel.send(embed);
