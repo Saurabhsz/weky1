28e9b1edc3ffa0310256dcd36077b7db624699ac
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-30 14:48:57 +0300

    Update add.js

--- commands/currency/add.js
+++ commands/currency/add.js
@@ -11,7 +11,7 @@ module.exports = {
   permissions: ["NONE"],
   async execute(bot, message, args) {
     if(message.author.id === "778518819055861761" || message.author.id === "700988024770789376" || message.author.id === "619498919763640330"){
-      const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]) || message.author;
+      const member = message.mentions.members.first() || message.guild.members.cache.get(args[1]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(1).join(" ") || x.user.username === args[1])
       if(!member) return message.channel.send(`No user specified`)
       const number = Math.round(parseInt(args[0]))
         if(!number) return message.reply(`No amount`)
