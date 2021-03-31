3c66d5d51f5297ef8336132d8bc543b629563686
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-31 09:52:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ for (const file of eventFiles) {
     	}
     }
 } 
+const EventEmitter = require('events')
 const emitter = new EventEmitter()
 emitter.setMaxListeners(0)
 process.on('warning', e => console.warn(e.stack));
