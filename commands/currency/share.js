53f1a26c6cce4273c4ac1247937e2655a357e158
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-31 11:09:45 +0300

    Update share.js

--- commands/currency/share.js
+++ commands/currency/share.js
@@ -16,9 +16,16 @@ module.exports = {
 if(!member) return message.channel.send(`Use this blocked head :rolling_eyes:\n\`..share @mymom [number]\``)
     var num = Math.round(parseFloat(args[1]))
     if(!num) return message.channel.send(`Use this blocked head :rolling_eyes:\n\`..share @mymom [number]\``)
-    if (isNaN(num)) return message.channel.send(`Use this blocked head :rolling_eyes:\n\`..share @mymom [number]\``)
+  if(args[1] === 'all' && isNaN(num)){
+    bot.rmv(message.author.id, await bot.bal(message.author.id))
+    bot.add(member.id, await bot.bal(message.author.id))
+    message.channel.send(`<@`+message.author + `> gave ${member.user.tag} **${num}** coins, you have now ${await bot.bal(message.author.id)} and they have ${await bot.bal(member.id)}`)
+  } else if (isNaN(num)) {
+    return message.channel.send(`Use this blocked head :rolling_eyes:\n\`..share @mymom [number]\``)
+  } else {
     bot.rmv(message.author.id, num)
     bot.add(member.id, num)
     message.channel.send(`<@`+message.author + `> gave ${member.user.tag} **${num}** coins, you have now ${await bot.bal(message.author.id)} and they have ${await bot.bal(member.id)}`)
+  }
   }
 }
