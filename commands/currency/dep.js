af17217934c2daeaae80e2000b4bdc7103fe2a9b
ImFace <78975742+ImFace@users.noreply.github.com>
2021-03-31 09:16:28 +0300

    Update dep.js

--- commands/currency/dep.js
+++ commands/currency/dep.js
@@ -7,12 +7,11 @@ module.exports = {
   guildOnly: true, //or false
   usage: '..deposit',
   cooldown: 4, //seconds(s)
-  cooldowny: 1,
   guarded: true, //or false
   permissions: ["NONE"],
   async execute(bot, message, args) {
-
-    var num = parseFloat(args[0])
+    var num = parseInt(args[0])
+    if (num.isNaN) return message.channel.send("Thats not a valid number");
     const Money = require('../../schemas/Money')
 Money.findOne({
   id: message.author.id
@@ -25,47 +24,16 @@ Money.findOne({
     newD.save();
     let user = message.guild.members.cache.get(message.author.id);
     user.user.send(`Hello , **thanks for starting using Weky Bot**!\n You got 100 coins as reward for starting. Do \`/help\` for more commands about our currency system.`)
-  } else if(num >= data.Wallet) {
+  } else if(num > data.Wallet) {
     return message.channel.send("You dont have that much money why you bad at me bro :/");
   }
-  const thesame = data.Bank >= data.space
-  if(thesame){
-    data.Bank -= data.space
-    data.Wallet += data.Bank - data.space
-    data.save()
-  }else if(Math.round(num) >= Math.round(data.space)){
-      return message.channel.send(`Looks like your bank is full :) go get some scripts`)
-     } else {
-      if(args[0] === 'all' && isNaN(num)) {
-if(data.space < Math.round(data.Wallet)) return message.channel.send(`You cannot deposit that much coins, your bank is full`)
-        data.Bank += Math.round(data.Wallet)
-        message.channel.send("Deposited **" + Math.round(data.Bank) + '** coins.')
-        data.Wallet -= Math.round(data.Wallet)
-        data.save()
-        } else if(args[0] === 'half' && isNaN(num)){
-    if(data.space < Math.round(data.Wallet/2)) return message.channel.send(`You cannot deposit that much coins, your bank is full`)
-            data.Bank += Math.round(data.Wallet/2)
-            message.channel.send("Deposited **" + Math.round(data.Wallet/2) + '** coins.')
-            data.Wallet -= Math.round(data.Wallet/2)
-            data.save()
-            } else if(isNaN(num)){
-              return message.channel.send(`You didnt said \`all\`, \`half\` or ,\`number\``)
-
-        } else if(!isNaN(num)){
-        if(Math.round(num) >= Math.round(data.space)){
-          return message.channel.send(`Looks like your bank is full :) go get some scripts`)
-        }else if(thesame){
-          data.Bank -= data.space
-          data.Wallet += data.Bank - data.space
-          data.save()
-        } else {
-          if(Math.round(num) >= data.space || data.space === data.Bank) return message.channel.send(`You cannot deposit that much coins, your bank is full`)
-          data.Wallet -= Math.round(num)
-          data.Bank += Math.round(num)
-          data.save();
-          message.channel.send("Deposited **" + Math.round(num) + '** coins.')
-        }
-      }
+  if(num < data.space){
+   return message.channel.send(`Looks like your bank is full :) go get some scripts`)
+  } else {
+    data.Wallet -= num;
+   data.Bank += num;
+   data.save();
+   message.channel.send("Deposited **" + num + '** coins.')
 }
 });
 }
