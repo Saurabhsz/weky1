c67a4113be9dcf49da522b0258ce1ed5a638009d
ImFace <78975742+ImFace@users.noreply.github.com>
2021-04-08 18:15:27 +0300

    ok

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -10,80 +10,42 @@ module.exports = {
     guarded: true, //or false
     permissions: ["ONLY_IN_WEKY_SUPPORT_SERVER"],
     async execute(bot, message, args) {
-    if (message.guild.id !== '795393018764591134') return message.reply('This command can only be used in another server.');
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You do not have the correct permissions to use this command.')
+        const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        const time = args[1]
+        if(!Member) return message.channel.send('Member is not found!')
+        if(!time) return message.channel.send('Please specify a time.')
+        console.log(isNaN(time))
+        const role = message.guild.roles.cache.find(role => role.name.toLowerCase() === 'Muted')
+        if(!role){
+            try{
+                message.channel.send('Muted role is not found, creating muted role...')
 
-    let Discord = require('discord.js')
-//checking if the member has the mute perms
-if (!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("You can not use this command");
-//checking if the bot has the mute perms
-if (!message.guild.me.hasPermission("MUTE_MEMBERS")) return message.channel.send("I do not have \`MUTE_MEMBERS\` permission.");
-
-const mentionedMember = message.mentions.members.first();
-let reason = args.slice(2).join(" ");
-let time = args[1];
-
-
-//checking and making a mute role
-const role = message.guild.roles.cache.find(role => role.id === '814569175284318270')
-if (!role) {
-    try {
-        message.channel.send('Muted role is not found, attempting to create muted role.');
-
-        let muterole = await message.guild.roles.create({
-            data: {
-                name: 'Muted',
-                permissions: []
+                let muterole = await message.guild.roles.create({
+                    data : {
+                        name: 'Muted',
+                        permissions: []
+                    }
+                });
+                message.guild.channels.cache.filter(c => c.type === 'text').forEach(async (channel, id) => {
+                    await channel.createOverwrite(muterole, {
+                        SEND_MESSAGES: false,
+                        ADD_REACTIONS: false
+                    })
+                });
+                message.channel.send('Muted role has successfully been created.')
+            } catch (error) {
+                console.log(error)
             }
-        });
-        message.guild.channels.cache.filter(c => c.type === 'text').forEach(async (channel, id) => {
-            channel.updateOverwrite(muterole, {
-                SEND_MESSAGES: false,
-                VIEW_CHANNEL: true
-            })
-        });
-        message.channel.send('Muted role has sucessfully been created.');
-    } catch (error) {
-        console.log(error)
-    }
-};
-const muteRole = message.guild.roles.cache.find(role => role.id === '814569175284318270')
+        };
+        let role2 = message.guild.roles.cache.find(r => r.name.toLowerCase() === 'Muted')
+        if(Member.roles.cache.has(role2.id)) return message.channel.send(`${Member.user.tag} has already been muted`)
+        await Member.roles.add(role2)
+        message.channel.send(`${Member.displayName} is now muted`)
 
-
-
-if (!time) return message.channel.send('You must state a duration tempmute with a duration of time. \`-.mute @member time reason\`');
-//checking if the args 0 is provided
-if (!args[0]) return message.channel.send("\`.mute @member time reason\`");
-//checking if there is a mentioned member
-if (!mentionedMember) return message.channel.send("Please state a member that is in the server to mute");
-//checking if the user using this command is using it on themself
-if (mentionedMember.user.id === message.author.id) return message.channel.send("You can not mute yourself");
-//checking if the user has used the command on a bot
-if (mentionedMember.user.id === bot.id) return message.channel.send("NO");
-//automatically changing the reason if none given
-if (!reason) reason = "No reason given";
-//checking if the member is alredy muted
-if (mentionedMember.roles.cache.has(muteRole.id)) return message.channel.send("This user is alredy muted");
-//checking if the mentionedMember has a higher role then the autor of the message
-if (message.member.roles.highest.position <= mentionedMember.roles.highest.position) return message.channel.send("You are not higher than that user.");
-if (mentionedMember.hasPermission("ADMINISTRATOR")) return message.channel.send("I cannot mute this user.");
-//making an embed to send in user's dms
-const muteEmbed = new Discord.MessageEmbed()
-    .setTitle(`You were muted in ${message.guild.name}.`)
-    .addField(`Reason`, `${reason}`)
-    .setTimestamp()
-    .setColor("RANDOM")
-    .setFooter(bot.user.tag, bot.user.displayAvatarURL());
-
-
-//making an embed to send in chat
-message.channel.send(`<@${mentionedMember.id}> has been muted for ${args[1]} due **${reason}**`)
-await mentionedMember.send(`You have been muted for ${args[1]} due **${reason}**`)
-
-    await mentionedMember.roles.add(muteRole);
-
-setTimeout(async function () {
-    await mentionedMember.roles.remove(muteRole).then(message.channel.send(`<@${mentionedMember.id}> has been unmuted`));
-    await mentionedMember.send(`You have been unmuted`).catch(err => console.log(err));
-}, ms(time));
+        setTimeout(async () => {
+            await Member.roles.remove(role2)
+            message.channel.send(`${Member.user.tag} is now unmuted`)
+        }, ms(time))
     }
 }
--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -8,7 +8,7 @@ module.exports = {
     cooldown: 4, //seconds(s)
     cooldowny: 0,
     guarded: true, //or false
-    permissions: ["ONLY_IN_WEKY_SUPPORT_SERVER"],
+    permissions: ["MANAGE_MESSAGES"],
     async execute(bot, message, args) {
 
     let Discord = require('discord.js')
@@ -16,14 +16,14 @@ module.exports = {
 if (!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("You can not use this command");
 //checking if the bot has the mute perms
 if (!message.guild.me.hasPermission("MUTE_MEMBERS")) return message.channel.send("I do not have \`MUTE_MEMBERS\` permission.");
-if (message.guild.id !== '795393018764591134') return message.reply('This command can only be used in another server.');
 const mentionedMember = message.mentions.members.first();
 
-const muteRole = message.guild.roles.cache.find(role => role.id === '814569175284318270')
+const muteRole = message.guild.roles.cache.find(role => role.name.toLowerCase() === 'Muted')
+if(!muteRole) return;
 if (!mentionedMember) return message.channel.send("Please state a member that is in the server to mute");
 
     await mentionedMember.roles.remove(muteRole).then(message.channel.send(`<@${mentionedMember.id}> has been unmuted`));
-    await mentionedMember.send(`You have been unmuted`).catch(err => console.log(err));
+    await mentionedMember.send(`You have been unmuted.`).catch(err => console.log(err));
 
     }
 }
