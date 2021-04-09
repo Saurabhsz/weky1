2c955e5f6cc173d81037bb46476ef613da9cab3b
ImFace <78975742+ImFace@users.noreply.github.com>
2021-04-09 19:12:49 +0300

    Update lbrank.js

--- commands/level/lbrank.js
+++ commands/level/lbrank.js
@@ -16,7 +16,7 @@ const Canvas = require('canvas');
     const canvas = Canvas.createCanvas(867, 892);
     const ctx = canvas.getContext('2d');
     const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10);
-    const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard);     
+    const leaderboard = await Levels.computeLeaderboard(bot, rawLeaderboard);     
     if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
 
     const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
