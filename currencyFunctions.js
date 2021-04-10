107513cebc6fd724bd23f564694fc7fd186efc20
ImFace <78975742+ImFace@users.noreply.github.com>
2021-04-10 12:55:56 +0300

    Update currencyFunctions.js

--- currencyFunctions.js
+++ currencyFunctions.js
@@ -52,13 +52,14 @@ inventory.findOne({User: id},(err, b) => {
   if(!b) {
 bot.createProfile(id)
   }
-if(b.BoosterEffect !== 0){brr = `${brr+'0.10'}`;
+if(b.BoosterEffect !== 0){brr += 0.10;
     b.BoosterEffect -= 1;
     b.save()
 }
 if(message.guild.id === '830003680983646278') {
-  brr = `${brr+'0.30'}`
+  brr += 0.30
 }
+console.log(brr)
 if(brr === ''){
     data.Wallet += amount
     data.save()
