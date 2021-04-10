f4408991da6cde8961f1863e04a859bd28f130d4
ImFace <78975742+ImFace@users.noreply.github.com>
2021-04-10 19:15:49 +0300

    Update lbrank.js

--- commands/level/lbrank.js
+++ commands/level/lbrank.js
@@ -24,7 +24,7 @@ const Canvas = require('canvas');
 
     const lb = leaderboard.map(e => `${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
 
-    const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/830003681402683419/830469537475330078/Untitled.jpg');
+    const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/797153663088590911/830474905611010109/Untitled_1.jpghttps://cdn.discordapp.com/attachments/797153663088590911/830474905611010109/Untitled_1.jpg');
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
 	ctx.strokeStyle = '#74037b';
