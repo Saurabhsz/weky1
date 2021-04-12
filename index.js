915c34f610e372635b3723cd24c07c7918d96f05
ImFace <78975742+ImFace@users.noreply.github.com>
2021-04-12 16:08:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -306,6 +306,8 @@ const prefix = data.prefix
                               return
                           }
                           let datab = require('./schemas/cmds').findOne({ Guild: message.guild.id}, async (err, data) => {
+                            if(!data){ new datab({Guild: message.guild.id}).save()}
+
                             if(!data.Cmds.includes(cmd)){
                               cmd.execute(bot, message, args);
                             } else {
