09fc4c64f9700bca13dfd35d282ba7593e4734b3
ImFace <78975742+ImFace@users.noreply.github.com>
2021-04-14 10:22:45 +0300

    Update sudo.js

--- commands/fun/sudo.js
+++ commands/fun/sudo.js
@@ -14,12 +14,12 @@ module.exports = {
     guarded: true, //or false
     permissions: ["NONE"],
     async execute(bot, message, args) {
-        const { sudo } = require('weky')
+        const weky = require('weky')
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
         if (!member) return message.reply(`Couldn't find this user!`)
         const msg = args.slice(1).join(" ")
         if(!msg) return message.reply('What should the user say?')
-        const sud = new sudo({
+        const sud = new weky.sudo({
             message: message,
             text: msg,
             member: member,
