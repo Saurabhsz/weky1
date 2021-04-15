6939d780c489cb9f6a7a4a09c42b41dd50b7347c
ImFace <78975742+ImFace@users.noreply.github.com>
2021-04-15 12:31:59 +0300

    e

--- commands/games/fasttype.js
+++ commands/games/fasttype.js
@@ -13,46 +13,15 @@ module.exports = {
     guarded: true, //or false
     permissions: ["NONE"],
     async execute(bot, message, args) {
-        const filter = m => m.author.id === message.author.id
-        if (inGame.has(message.author.id)) return
-        inGame.add(message.author.id)
-        for (i = 0; i < 25; i++) {
-            const time = Date.now()
-            let sentence = ''
-            let ogSentence = txtgen.sentence().toLowerCase().split('.').join('').split(',').join('')
-            ogSentence.split(' ').forEach(argument => {
-                sentence += '`' + argument.split('').join(' ') + '` '
-            })
-            message.reply(`**Write the following message (You have 50s seconds!)**:\n${sentence}`)
-            try {
-                msg = await message.channel.awaitMessages(filter, {
-                    max: 1,
-                    time: 50000,
-                    errors: ['time']
-                })
-            } catch (ex) {
-                message.reply('Time\'s up!')
-                inGame.delete(message.author.id)
-                break
-            }
-            if (['cancel', 'end'].includes(msg.first().content.toLowerCase().trim())) {
-                message.channel.send('Ended!')
-                inGame.delete(message.author.id)
-                break
-            } else if (msg.first().content.toLowerCase().trim() === ogSentence.toLowerCase()) {
-                message.channel.send(`Good job!\nIt took you ${ms(Date.now() - time, {long: true})} to type it!`)
-                bot.ADDfsWin(message.author.id)
-            } else {
-                message.channel.send('You failed!')
-                inGame.delete(message.author.id)
-                break
-            }
-
-            if (i === 25) {
-                message.reply('GG! You win!')
-                inGame.delete(message.author.id)
-                break
-            }
-        }
+        const { FastType } = require('weky')
+const game = new FastType({
+    message: message,
+    winMessage: "GG you won!", //message sent when user types perfectly
+    sentence: txtgen.sentence(), //sentence-to-be-typed
+    loseMessage: 'Lost ;(', //message sent when user misspell it
+    time: 50000, //time that user has in ms
+    startMessage: 'Good Luck!' //message sent when user starts playing
+})
+game.start()
 }
 }
