c2bd41eddb953487da6b1e4b00668786edae4498
ImFace <78975742+ImFace@users.noreply.github.com>
2021-04-24 10:18:36 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,4 @@
 //ready event (ready.js)
 module.exports = (bot, message) => {
-        commandFiles.forEach(cmd => {
-            console.log('> ' + `${folder} | ` + `${cmd}`)
-        })
-        console.log(`----------------------------------------\n¡${bot.user.tag} has been successfully connected!`)
-        bot.user.setActivity(`Playing in ${bot.guilds.cache.size} servers with ${bot.users.cache.size} users | ..vote`, {type: "PLAYING"});
+       bot.user.setActivity(`Playing in ${bot.guilds.cache.size} servers with ${bot.users.cache.size} users | ..vote`, {type: "PLAYING"});
     }
