50c76fd51d1cb65c1f5dbcd04e407cbcfd2e2b3d
ImFace <78975742+ImFace@users.noreply.github.com>
2021-04-28 10:14:13 +0300

    Update stats.js

--- commands/utility/stats.js
+++ commands/utility/stats.js
@@ -1,10 +1,11 @@
 
+ const { MessageEmbed } = require('discord.js');
 const config = require('../../util/config.json');
 
 module.exports.run = async (client, message, args, utils, data) => {
     const prettyMs = require('pretty-ms')
     const msg = await message.reply(`Loading...`).then(m  => m.delete(), 1000)
-let embed = new Discord.MessageEmbed()
+let embed = new MessageEmbed()
 .setAuthor(`Weky bot stats`)
 .addField(`__Cache__\n`,`**Guilds**: ${bot.guilds.cache.size} \n\n`, true)
 .addField(`__Uptime__\n`,`**Bot**: ${bot.uptime}\n**Process**: ${prettyMs(process.uptime() * 1000)}`, true)
