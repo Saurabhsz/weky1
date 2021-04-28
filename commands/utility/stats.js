59b8cd72f0a0bdd6d7908fe9ac58eb7b03ad4288
ImFace <78975742+ImFace@users.noreply.github.com>
2021-04-28 10:35:51 +0300

    Update stats.js

--- commands/utility/stats.js
+++ commands/utility/stats.js
@@ -7,9 +7,9 @@ module.exports.run = async (client, message, args, utils, data) => {
     const msg = await message.reply(`Loading...`).then(m  => m.delete(), 1000)
 let embed = new MessageEmbed()
 .setAuthor(`Weky bot stats`)
-.addField(`__Cache__\n`,`**Guilds**: ${bot.guilds.cache.size} \n\n`, true)
-.addField(`__Uptime__\n`,`**Bot**: ${bot.uptime}\n**Process**: ${prettyMs(process.uptime() * 1000)}`, true)
-.addField(`__Movement__\n`,`**API Latency**: ${Math.round(bot.ws.ping)}\n**Bot Latency**: ${msg.createdTimestamp - message.createdTimestamp}ms`, true)
+.addField(`__Cache__\n`,`**Guilds**: ${client.guilds.cache.size} \n\n`, true)
+.addField(`__Uptime__\n`,`**Bot**: ${client.uptime}\n**Process**: ${prettyMs(process.uptime() * 1000)}`, true)
+.addField(`__Movement__\n`,`**API Latency**: ${Math.round(client.ws.ping)}\n**Bot Latency**: ${msg.createdTimestamp - message.createdTimestamp}ms`, true)
 message.reply(embed)
 };
 
