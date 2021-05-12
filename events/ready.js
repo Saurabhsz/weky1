629e22cde5bad26c25b7f75f9874ea43cd102b10
ImFace <78975742+ImFace@users.noreply.github.com>
2021-05-12 15:06:01 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,29 @@
 //ready event (ready.js)
 module.exports = (bot, message) => {
+   const top = require('top.gg-core');
+
+   const client = new discord.Client();
+   const topgg = new top.Client(process.env.topgg_token);
+   const webhook = new top.Webhook(process.env.topgg_pass);
+   let votes = await topgg.votes(); 
+     topgg.post({
+       servers: client.guilds.cache.size
+     });
+   
+     setInterval(() => {
+       topgg.post({
+         servers: client.guilds.cache.size
+       });
+     }, 3600000);
+   
+   topgg.on('posted', data => {
+     console.log(data);
+   });
+   
+   webhook.login('/topggVote', '3000'); //you can edit the port (only numbers) NOTE: PUT .login METHOD BEFORE THE .on('vote') METHOD
+   
+   webhook.on('vote', vote => {
+     console.log(`User id: ${vote.user}\nAll data: ${vote}\nTotal votes: ${votes}`);
+   });
        bot.user.setActivity(`Playing in ${bot.guilds.cache.size} servers with ${bot.users.cache.size} users | ..vote`, {type: "PLAYING"});
     }
