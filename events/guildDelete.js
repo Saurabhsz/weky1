1cb5da742e6fb2beb206b3133fc8765d0b8c6d5a
yCyanx <70500963+yCyanxW@users.noreply.github.com>
2021-05-13 14:24:30 +0800

    REMOVE REMOVE -> DELETE :Man:
    xd

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,7 +4,9 @@ module.exports = async (client, guild) => {
 	const channel = await client.channels.cache.get('835142255793930251');
 	const m = new Discord.MessageEmbed()
 		.setTitle(`Left ${guild.name}`)
+	        .setThumbnail(guild.iconURL({ dynamic: true, size: 1024}))
 		.setFooter(`Total servers : ${client.guilds.cache.size} | Members : ${guild.memberCount} | Owner: ${guild.owner}`)
+	        .setTimestamp()
 		.setColor('RED');
 	channel.send(m);
 };
