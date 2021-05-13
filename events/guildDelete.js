770bee6c499617d325a8535502b570b6215f8f5c
yCyanx <70500963+yCyanxW@users.noreply.github.com>
2021-05-13 14:51:11 +0800

    try
    try

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,12 +1,10 @@
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = async (client, guild) => {
-	const channel = await client.channels.cache.get('835142255793930251');
-	const m = new Discord.MessageEmbed()
+	const channel = await client.channels.cache.get('835142227545686086');
+	const m = new MessageEmbed()
 		.setTitle(`Left ${guild.name}`)
-	        .setThumbnail(guild.iconURL({ dynamic: true, size: 1024}))
 		.setFooter(`Total servers : ${client.guilds.cache.size} | Members : ${guild.memberCount} | Owner: ${guild.owner}`)
-	        .setTimestamp()
 		.setColor('RED');
 	channel.send(m);
 };
