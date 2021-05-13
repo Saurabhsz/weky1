9c106662585c8019aec89ba5c8fac9a95ab21789
yCyanx <70500963+yCyanxW@users.noreply.github.com>
2021-05-13 12:43:57 +0800

    xd
    xd

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,10 +1,10 @@
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
-module.exports = async (bot, guild) => {
-	const channel = await bot.channels.cache.get('835142227545686086');
-	const m = new Discord.MessageEmbed()
+module.exports = async (client, guild) => {
+	const channel = await client.channels.cache.get('835142227545686086');
+	const m = new MessageEmbed()
 		.setTitle(`Joined ${guild.name}`)
-		.setFooter(`Total servers : ${bot.guilds.cache.size} | Members : ${guild.memberCount} | Owner: ${guild.owner}`)
+		.setFooter(`Total servers : ${client.guilds.cache.size} | Members : ${guild.memberCount} | Owner: ${guild.owner}`)
 		.setColor('GREEN');
 	channel.send(m);
 };
