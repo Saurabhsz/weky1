b021d8e8150ba60e53540a09e52155a632e0c1eb
yCyanx <70500963+yCyanxW@users.noreply.github.com>
2021-05-13 14:34:01 +0800

    Join Weky! discord.gg/rysqzyVVRK
    xd

--- commands/alt detector/alt-actions.js
+++ commands/alt detector/alt-actions.js
@@ -11,9 +11,9 @@ module.exports.run = async (client, message, args, utils, data) => {
 
 
   let choices = ["none", "kick", "ban"]
-  if (!args[0]) return message.channel.send(new MessageEmbed().setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png' })).setDescription(`${message.client.emoji.fail} ${language.aactionNotValidChoice.replace("{allChoices}", choices.join(", "))}`).setFooter('https://pogy.xyz').setTimestamp().setColor('RED'));
+  if (!args[0]) return message.channel.send(new MessageEmbed().setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png' })).setDescription(`${message.client.emoji.fail} ${language.aactionNotValidChoice.replace("{allChoices}", choices.join(", "))}`).setFooter('Join Weky! discord.gg/rysqzyVVRK').setTimestamp().setColor('RED'));
 
-  if (!choices.includes(args[0].toLowerCase())) return message.channel.send(new MessageEmbed().setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png' })).setDescription(`${message.client.emoji.fail} ${language.aactionNotValidChoice.replace("{allChoices}", choices.join(", "))}`).setFooter('https://pogy.xyz').setTimestamp().setColor('RED'));
+  if (!choices.includes(args[0].toLowerCase())) return message.channel.send(new MessageEmbed().setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png' })).setDescription(`${message.client.emoji.fail} ${language.aactionNotValidChoice.replace("{allChoices}", choices.join(", "))}`).setFooter('Join Weky! discord.gg/rysqzyVVRK').setTimestamp().setColor('RED'));
 
   await alt.findOne({ guildID: message.guild.id }, async (err, db) => {
     if (!db) {
@@ -29,13 +29,13 @@ module.exports.run = async (client, message, args, utils, data) => {
 
       await newGuild.save()
         .catch(err => { console.log(err) })
-      return message.channel.send(new MessageEmbed().setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png' })).setDescription(`${message.client.emoji.success} ${language.aactionSuccess.replace("{action}", args[0])}`).setFooter('https://pogy.xyz').setTimestamp().setColor('RED'));
+      return message.channel.send(new MessageEmbed().setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png' })).setDescription(`${message.client.emoji.success} ${language.aactionSuccess.replace("{action}", args[0])}`).setFooter('Join Weky! discord.gg/rysqzyVVRK').setTimestamp().setColor('RED'));
     }
     await db.updateOne({
       altAction: args[0].toLowerCase()
     })
 
-    return message.channel.send(new MessageEmbed().setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png' })).setDescription(`${message.client.emoji.success} ${language.aactionSuccess.replace("{action}", args[0])}`).setFooter('https://pogy.xyz').setTimestamp().setColor('RED'));
+    return message.channel.send(new MessageEmbed().setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png' })).setDescription(`${message.client.emoji.success} ${language.aactionSuccess.replace("{action}", args[0])}`).setFooter('Join Weky! discord.gg/rysqzyVVRK').setTimestamp().setColor('RED'));
 
   })
 };
